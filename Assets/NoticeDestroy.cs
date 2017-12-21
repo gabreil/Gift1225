2cc426bd832e63ecd66632fadd1a7f7045ed2b43
yishenyao <5428037@qq.com>
2017-12-21 11:51:35 +0800

    动态提示

---
+++ Assets/NoticeDestroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NoticeDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.GetComponent<Text> ().color.a == 0) {
+			Destroy (gameObject);
+		}
+	}
+}
--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -9,6 +9,7 @@ public class GameController : MonoBehaviour {
 	public static GameController gamecontroller;
 	public Sprite[] fish = new Sprite[13];
 	public Image fishimg;
+	public GameObject txt_notice;
 
 	void Awake(){
 		gamecontroller = this;
@@ -24,16 +25,27 @@ public class GameController : MonoBehaviour {
 		
 	}
 
+	//改鱼
 	public void ChangeFish(){
 		int id = Random.Range (0, 13);
 		fishimg.GetComponent<Image>().sprite = fish[id];
 	}
 
+	//显示提示
+	public void ShowNotice(string noticetxt){
+		GameObject notice = Instantiate (txt_notice);
+		notice.transform.SetParent(GameObject.Find ("UI_Notice").transform);
+		//notice.transform.position = new Vector3 (0.0f, 0.0f, 0.0f);
+		notice.GetComponent<Text> ().text = noticetxt;
+		Debug.Log (notice.GetComponent<Text> ().text);
+	}
+
+	//显示各种TIPS
 	public void DownExp(){
-		PlayerController.playercontroller.OnGetFood ();
+		ShowNotice ("test");
 	}
 	public void UpExp(){
-		PlayerController.playercontroller.CloseUI();
+		ShowNotice ("test1");
 	}
 
 }
